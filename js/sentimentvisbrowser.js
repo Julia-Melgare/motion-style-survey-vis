eae0e363b7bb68d9e24a8067e50c63b77c9f9a38
Julia <julia.melgare@acad.pucrs.br>
2022-10-17 20:29:39 -0300

    update margin

--- js/sentimentvisbrowser.js
+++ js/sentimentvisbrowser.js
@@ -993,7 +993,7 @@ function prepareLollipopChartData(){
 // Renders lollipop chart for word ranking
 function renderLollipopChart(){
 	lollipopChartData = prepareLollipopChartData();
-	var margin = {top: 15, right: 10, bottom: 15, left: 12};	
+	var margin = {top: 1, right: 10, bottom: 15, left: 30};	
 	var outerWidth = Math.round($("#lollipopChart").width());
 	var outerHeight = Math.round($("#lollipopChart").height());	
 	var canvasHeight = outerHeight - margin.top - margin.bottom;
